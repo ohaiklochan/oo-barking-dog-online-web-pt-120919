eb851f9309adf0a4b2d4cb9ce2637e33c001bacc
Chloe Foronda <caforonda103@yahoo.com>
2020-01-12 21:48:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,14 @@ class Dog
   def dog_speak
     @this_dog_speaks = dog_speak
   end
+end
   
-  fido = Dog.new
-  fido.name = "Fido"
+fido = Dog.new
+fido.name = "Fido"
   
+puts fido.name
+  
+woof = Dog.speak
+woof.speak = "Woof!"
+
+puts woof.speak
